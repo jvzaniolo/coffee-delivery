1fd825da4d85c2ca64a58919e07abd0d5b34585f
João Vitor <jv.zaniolo@gmail.com>
2023-10-24 16:25:58 -0300

    Add routes and react query

--- src/main.tsx
+++ src/main.tsx
@@ -7,10 +7,26 @@ import { Index } from './routes/_index'
 import { Checkout } from './routes/checkout'
 import { Order } from './routes/order'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RootLayout>
-      <Index />
-    </RootLayout>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Index />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/order" element={<Order />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>
 )
