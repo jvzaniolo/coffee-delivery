0604cda2418da59e5d74f23d2c0c4a3c2e4f553f
João Vitor <jv.zaniolo@gmail.com>
2023-10-24 16:26:19 -0300

    Change to client-side navigation

--- src/root.tsx
+++ src/root.tsx
@@ -1,12 +1,13 @@
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
+import { Link, Outlet } from 'react-router-dom'
 
-export function RootLayout({ children }: any) {
+export function RootLayout() {
   return (
     <>
       <header className="mx-auto flex max-w-[1120px] items-center justify-between px-4 py-8">
-        <a href="/" className="rounded-md p-2">
+        <Link to="/" className="rounded-md p-2">
           <img src="/images/logo.svg" alt="Coffee Delivery" />
-        </a>
+        </Link>
 
         <div className="flex items-center gap-3">
           <div className="flex items-center gap-1 rounded-md bg-purple-light fill-purple p-2 text-sm">
@@ -14,19 +15,19 @@ export function RootLayout({ children }: any) {
             <span>Ribeirão Preto, SP</span>
           </div>
 
-          <a
-            href="/checkout"
+          <Link
+            to="/checkout"
             className="relative flex items-center justify-center rounded-lg bg-yellow-light p-2 leading-none text-yellow-dark"
           >
             <ShoppingCart size={22} weight="fill" />
             <span className="absolute right-0 top-0 -translate-y-1/2 translate-x-1/2 rounded-full bg-yellow-dark px-[0.4rem] py-1 text-xs font-bold tabular-nums leading-none text-white">
               3
             </span>
-          </a>
+          </Link>
         </div>
       </header>
 
-      {children}
+      <Outlet />
     </>
   )
 }
